ba9e617a3a0bbcac7c78699d84282b4a4f0800a6
icebyrg <shildreturn@outlook.com>
2023-03-13 21:12:58 +0800

    实现属性的依赖收集和更新

--- vue3/vue3-lesson/packages/reactivity/src/effect.ts
+++ vue3/vue3-lesson/packages/reactivity/src/effect.ts
@@ -3,9 +3,17 @@ export let activeEffect = undefined
 export class ReactiveEffect {
   // 默认会将fn挂载到类的实例上
   constructor(private fn) {}
+  parent = undefined
+  deps = [] // 我依赖了哪些列表
   run() {
-    activeEffect = this
-    return this.fn()
+    try {
+      this.parent = activeEffect
+      activeEffect = this
+      return this.fn()
+    } finally {
+      activeEffect = this.parent
+      this.parent = undefined
+    }
   }
 }
 
@@ -13,4 +21,46 @@ export function effect(fn) {
   // 创建一个响应式effect 并且让effect执行
   const _effect = new ReactiveEffect(fn)
   _effect.run()
+}
+
+// 结构示例：{ name: 'xxx' } : 'name' -> [ effect, effect ]
+// 结构分析：weakMap : map : set
+
+const targetMap = new WeakMap()
+export function track(target, key) {
+  // 让这个对象上的属性 记录当前的activeEffect
+  if (activeEffect) {
+    // 说明用户是在effect中使用的这个数据
+    let depsMap = targetMap.get(target)
+    // 如果没有创建映射表
+    if (!depsMap) {
+      targetMap.set(target, (depsMap = new Map()))
+    }
+    // 如果有这个映射表 来查找一下有没有这个属性
+    let dep = depsMap.get(key)
+    // 如果没有set集合创建集合
+    if (!dep) {
+      depsMap.set(key, (dep = new Set()))
+    }
+    // 如果有则看一下set中有没有这个effect
+    let shouldTrack = dep.has(activeEffect)
+    if (shouldTrack) {
+      dep.add(activeEffect)
+      activeEffect.deps.push(dep)
+    }
+  }
+}
+
+export function trigger(target, key, newValue, oldValue) {
+  // 通过对象找到对应的属性 让这个属性对应的effect重新执行
+  const depsMap = targetMap.get(target)
+  if (!depsMap) {
+    return
+  }
+  const dep = depsMap.get(key) // name 或者 age 对应的所有effect
+  dep &&
+    dep.forEach((effect) => {
+      // 正在执行的effect 不要多次执行
+      if (effect !== activeEffect) effect.run
+    })
 }
--- vue3/vue3-lesson/packages/reactivity/src/handler.ts
+++ vue3/vue3-lesson/packages/reactivity/src/handler.ts
@@ -1,3 +1,4 @@
+import { track, trigger } from './effect'
 import { ReactiveFlags } from './reactive'
 
 export const mutableHandlers = {
@@ -6,16 +7,31 @@ export const mutableHandlers = {
     // 我们在使用proxy的时候要搭配reflect来使用，用来解决this问题
     // 取值的时候，让这个属性和effect产生关系
     // return target[key] 这样value里的内容没有被代理
+
+    // effect - 对应属性；建立映射关系
+    // console.log(activeEffect, key)
+
     if (key === ReactiveFlags.IS_REACTIVE) {
       return true
     }
-    return Reflect.get(target, key, receiver) // 类似于取属性用的call this从value改为proxy
+
+    const res = Reflect.get(target, key, receiver) // 类似于取属性用的call this从value改为proxy
+    // 做依赖收集 记录属性和当前effect的关系「多对多」
+    track(target, key)
+    return res
   },
   set(target, key, value, receiver) {
     // 更新数据
     // target[key] = value
     // return true
     // 找到这个属性对应的effect让它执行
-    return Reflect.set(target, key, value, receiver)
+    let oldValue = target[key]
+
+    const r = Reflect.set(target, key, value, receiver)
+
+    if (oldValue !== value) {
+      trigger(target, key, value, oldValue)
+    }
+    return r
   },
 }
