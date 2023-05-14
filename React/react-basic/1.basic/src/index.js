d66bdfb7ef0a3cb6d420b70070daf592d92241e5
icebyrg <shildreturn@outlook.com>
2023-05-14 17:15:33 +0800

    实现jsx

---
+++ React/react-basic/1.basic/src/index.js
@@ -0,0 +1,12 @@
+import React from './react'
+// import ReactDOM from 'react-dom/client'
+let element = (
+  <div className="title" style={{ color: 'red' }}>
+    <span>hello</span>
+  </div>
+)
+console.log(element)
+
+// const DOMRoot = ReactDOM.createRoot(document.getElementById('root'))
+
+// DOMRoot.render(element)
