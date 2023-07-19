01159cd191b5c6500d9f76d5968ef0a6838b9522
wren <wrenchou22@gmail.com>
2023-07-19 19:22:59 +0800

    vue3

--- vue3/packages/reactivity/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './effect'
-export * from './reactive'
-export * from './computed'
-export * from './watch'
-export * from './ref'
--- vue3/packages/shared/src/index.ts
+++
@@ -1,7 +0,0 @@
-export function isObject(val) {
-  return typeof val === 'object' && val !== null
-}
-
-export function isFunction(val) {
-  return typeof val === 'function'
-}
